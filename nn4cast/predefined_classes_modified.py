8e6441aa5349ea9684b5debaa3c31b682e880e23
Victorgf00 <116511981+Victorgf00@users.noreply.github.com>
2024-01-19 14:43:00 +0100

    Update predefined_classes_modified.py

--- nn4cast/predefined_classes_modified.py
+++ nn4cast/predefined_classes_modified.py
@@ -1,11 +1,39 @@
 #python file with all the functions created to the TFM model
 import sys
 
-# Add the directory containing the hyperparameters module or file to the search path
-sys.path.append('C:/Users/ideapad 5 15ITL05/Desktop/TFM/Predicciones_y_comparacion_ecmwf/model_library/')
+#import the necessary libraries to run the predefined_functions.py utilities
 
-# Import the necessary libraries
-from my_libraries import *
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import xarray as xr
+from scipy import stats as sts
+import scipy.stats as stats
+from scipy import signal
+from cartopy import crs as ccrs 
+import cartopy as car
+import numpy.linalg as linalg
+import numpy.ma as ma
+from scipy.stats import pearsonr
+import matplotlib.dates as mdates
+from cartopy.util import add_cyclic_point 
+import xskillscore as xs
+import time
+import random
+from tensorflow.keras.utils import plot_model
+from kerastuner.tuners import RandomSearch
+from sklearn.model_selection import KFold
+import os
+import shutil
+
+#The following two lines are coded to avoid the warning unharmful message.
+import warnings
+warnings.filterwarnings("ignore")
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import load_model
 plt.style.use('seaborn-v0_8')
 
 class ClimateDataPreprocessing:
