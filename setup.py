790a480c76fb4f310919c451237e968c51289d55
Victorgf00 <116511981+Victorgf00@users.noreply.github.com>
2024-01-19 13:28:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"": "nn4cast"},
     packages=setuptools.find_packages(where=""),
     install_requires=[
         'numpy==1.23.5', 'matplotlib==3.7.1', 'xarray==2023.2.0', 'cartopy==0.21.1', 'pandas==1.5.3', 'xskillscore==0.0.24', 'scipy==1.10.1', 'tensorflow==2.10.0', 'keras==2.10.0', 'kerastuner==1.3.4', 'scikit-learn==1.2.1'
